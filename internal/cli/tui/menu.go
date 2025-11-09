7c52acc2a3e75e26346acd39b60698d32db61d63
AbinashNagendran <abinashnagendran@gmail.com>
2025-11-08 19:31:18 -0500

    chore: add comments in menu.go

--- internal/cli/tui/menu.go
+++ internal/cli/tui/menu.go
@@ -5,13 +5,13 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 )
-
+// Model for application main page
 type MainMenuModel struct {
 	options   []string
 	selected  int
 	confirmed bool
 }
-
+// returns application initial model
 func NewMainMenu() *MainMenuModel {
 	return &MainMenuModel{
 		options: []string{
@@ -23,13 +23,16 @@ func NewMainMenu() *MainMenuModel {
 		selected: 0,
 	}
 }
-
+// returns intial command for the application to run
 func (m *MainMenuModel) Init() tea.Cmd {
+	// Note: For now, we don't have any initial I/O commands to do. nil = "no command"
 	return nil
 }
 
+// Handles incoming events and updates the model accordingly
 func (m *MainMenuModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	// Check if a key pressed, if so do the corresponding update to model
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "up", "k":
@@ -50,11 +53,11 @@ func (m *MainMenuModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 	return m, nil
 }
-
+// renders UI, looks at model at current state, return string s which is the UI 
 func (m *MainMenuModel) View() string {
 	s := "\n"
 	s += TitleStyle.Render("🚀 StockNet") + "\n\n"
-
+	// highlight the selected option with a →
 	for i, option := range m.options {
 		if i == m.selected {
 			s += fmt.Sprintf("%s\n", SelectedStyle.Render("→ "+option))
@@ -62,7 +65,8 @@ func (m *MainMenuModel) View() string {
 			s += fmt.Sprintf("%s\n", UnselectedStyle.Render("  "+option))
 		}
 	}
-
+	// The footer
 	s += FooterStyle.Render("↑/↓ or k/j to navigate • Enter to select • q to quit") + "\n\n"
+	
 	return s
 }
